5b3a21facf108843646855ed81dcc36b2c7cae0c
ooparshina <ooparshina@gmail.com>
2024-06-10 23:42:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+o = "ready steady go"
+print(len(o))
+
+first = 65
+second = 24
+summa = (first + second)
+diff = (first - second)
+print(summa), print(diff)
+
+third = 30
+mean = ((first + second + third)/3)
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник, '
+print((second_string),(first_string))
+
+a = 5
+b = 8
+c = 4
+f = ((a * b) + (a * c))
+print((f ** 3) / 2)
+
+
+
+
+
+
+
+
